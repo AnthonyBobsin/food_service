873cfd6b51b514a321afe632901f5522baba1004
AnthonyBobsin <bobsinj@gmail.com>
2014-12-07 13:54:19 -0500

    Added a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def full_title(title = '') 
+		base_title = "Food Service Application"
+		if title.empty?
+      base_title
+    else
+      "#{title} | #{base_title}"
+    end
+  end
+
 end
