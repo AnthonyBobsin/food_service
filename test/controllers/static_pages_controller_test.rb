94882c7b583075deab5e60d5e4e92f3e1be578a7
AnthonyBobsin <bobsinj@gmail.com>
2014-12-07 13:47:04 -0500

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Food Service Application"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Food Service Application"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "title", "About | Food Service Application"
   end
 
 end
