25b48d2bc5c588e0f82f3b0980d44327e36be1a8
AnthonyBobsin <bobsinj@gmail.com>
2014-12-08 01:30:19 -0500

    Finished main layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_select "title", "About | Food Service Application"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Food Service Application"
+  end
+
 end
